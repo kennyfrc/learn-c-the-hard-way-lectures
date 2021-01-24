fc89bba951554d534385017b38b3fd9fd3019582
kennyfrc <fxkennyfrc@gmail.com>
2021-01-24 23:32:24 +0800

    ex18: function pointers + bubble sort
    the key idea here is that you can declare a function pointer then use it as an argument to another function, like a callback
    
    when you're finally invoking the function that uses the callback as the argument, you can then pass on the actual name of the defined function.
    
    the key concept here to help you delineate between the two is declare vs define..
    
    this exercise also covers bubble sort which is interesting. mainly
    - compare two adjacent values
    - if one is bigger, bubble it up to the right (m-1 times))
    - you do passes n times, where n is the size of the array

--- ex18/ex18.c
+++ ex18/ex18.c
@@ -1,3 +1,15 @@
+/**
+ * Some key Ideas
+ * 1. function pointer (non-callbackable): 
+ * `int func(a,b) -> int (*func)(a,b) -> int (*new_name)(a,b)`
+ * 1.a. you can use this to cast them into functions
+ * 1.b. inuition is that you PASS a defined function to a function pointer that has been undefined but with allocated memory
+ * 
+ * 2. function pointer (callbackable): the type is now a new_name 
+ * `typedef int (*new_name)(a,b)`
+ * 3. memcpy (no care nul) vs strcpy
+**/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -15,26 +27,44 @@ void die(const char *message)
     exit(1);
 }
 
-// a typedef creates a fake type, in this
-// case for a function pointer
+/* this makes compare_cb a function pointer that 
+ * you can pass as a argument
+*/
 typedef int (*compare_cb) (int a, int b);
 
 /**
  * A classic bubble sort function that uses the 
  * compare_cb to do the sorting. 
+ * variable name of the pointer is compare_cb
+ * and you use it like a function
  */
 int *bubble_sort(int *numbers, int count, compare_cb cmp)
 {
     int temp = 0;
     int i = 0;
     int j = 0;
+
+    /* because this target will "work hard", it's 
+     * best to put this at heap memory
+    **/
     int *target = malloc(count * sizeof(int));
 
     if (!target)
         die("Memory error.");
 
+    /* the objective of memcpy is to copy a string
+     * while ignoring the /0 nul character
+     * in contrast, strcpy doesn't care
+     */
     memcpy(target, numbers, count * sizeof(int));
 
+    /* 
+     * this is the classic bubble sort
+     * - one pass will bubble up the biggest 
+     * element to the end
+     * - therefore, if an array is size n, then
+     * you want to bubble it up n times
+     */
     for (i = 0; i < count; i++) {
         for (j = 0; j < count - 1; j++) {
             if (cmp(target[j], target[j + 1]) > 0) {
@@ -74,16 +104,24 @@ int strange_order(int a, int b)
 void test_sorting(int *numbers, int count, compare_cb cmp)
 {
     int i = 0;
+    /* bubble sort allocates memory 
+     * - the best clue is the pointer
+     */
     int *sorted = bubble_sort(numbers, count, cmp);
 
+    // check as usual
     if (!sorted)
         die("Failed to sort as requested.");
 
+    // print the numbers
     for (i = 0; i < count; i++) {
         printf("%d ", sorted[i]);
     }
+
+    // end line
     printf("\n");
 
+    // free the memory
     free(sorted);
 }
 
@@ -100,6 +138,10 @@ void destroy(compare_cb cmp)
     printf("\n");
 }
 
+/* converts function to a string
+ * then it prints out its context in hex code 
+ * this shows the raw assembler bytecode
+ */ 
 void dump(compare_cb cmp)
 {
     int i = 0;
@@ -119,31 +161,34 @@ int main(int argc, char *argv[])
 {
     if (argc < 2) die("USAGE: ex18 4 3 1 5 6");
 
+    // number of actual arguments
     int count = argc - 1;
     int i = 0;
+    // pointer to a pointer + 1
+    // this essentially has moved it to the first argument
     char **inputs = argv + 1;
 
+    // initialize an array using the heap
     int *numbers = malloc(count * sizeof(int));
     if (!numbers) die("Memory error.");
 
+    // assign the arguments to the array
     for (i = 0; i < count; i++) {
         numbers[i] = atoi(inputs[i]);
     }
 
+    // pass the sorted_order, reverse_order, and strange_order functions
     test_sorting(numbers, count, sorted_order);
     test_sorting(numbers, count, reverse_order);
     test_sorting(numbers, count, strange_order);
 
+    printf("SORTED: ");
+    dump(sorted_order);
+
+    printf("REVERSE: ");
+    dump(reverse_order);
+
     free(numbers);
-
-    printf("SORTED:");
-    dump(sorted_order);
-
-    destroy(sorted_order);
-
-    printf("SORTED:");
-    dump(sorted_order);
-
 
     return 0;
 }
